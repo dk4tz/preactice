057d0e2d11894c4595aa500e53a673f1591c9639
david katz <dkatz@davids-mbp.myfiosgateway.com>
2022-10-20 07:06:49 -0400

    add prettier.rc, add ExpenseItem.css

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -3,9 +3,7 @@ import ExpenseItem from "./components/ExpenseItem";
 function App() {
   return (
     <div className="App">
-      <h1 className="App-header">
-        Let's get started!
-      </h1>
+      <h1 className="App-header">Let's get started!</h1>
       <ExpenseItem></ExpenseItem>
     </div>
   );
