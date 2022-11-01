839c0be4dfa698ba1a4944540468b2fef7cfdc42
david katz <dkatz@davids-mbp.myfiosgateway.com>
2022-11-01 07:28:01 -0400

    add ExpensesList component

--- expense-tracker/src/components/Expenses/Expenses.js
+++ expense-tracker/src/components/Expenses/Expenses.js
@@ -1,33 +1,32 @@
 import { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesList from "./ExpensesList";
 import ExpensesFilter from "./ExpensesFilter";
 
+
 function Expenses(props) {
-  const [filteredYear, setFilteredYear] = useState("2022");
+    const [filteredYear, setFilteredYear] = useState("2022");
 
-  const filterChangeHandler = (selectedYear) => {
-    setFilteredYear(selectedYear);
-  };
+    const filterChangeHandler = (selectedYear) => {
+        setFilteredYear(selectedYear);
+    };
 
-  return (
-    <div>
-      <Card>
-        <ExpensesFilter
-          selected={filteredYear}
-          onChangeFilter={filterChangeHandler}
-        />
-        {props.items.map((expense) => (
-          <ExpenseItem
-            title={expense.title}
-            amount={expense.amount}
-            date={expense.date}
-          />
-        ))}
-      </Card>
-    </div>
-  );
+    const filteredExpenses = props.items.filter((expense) => {
+        return expense.date.getFullYear().toString() === filteredYear;
+    });
+
+    return (
+        <div>
+            <Card>
+                <ExpensesFilter
+                    selected={filteredYear}
+                    onChangeFilter={filterChangeHandler}
+                />
+                <ExpensesList items={filteredExpenses} />
+            </Card>
+        </div>
+    );
 }
 
 export default Expenses;
