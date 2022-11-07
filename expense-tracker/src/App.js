b1aa6bebcbb66f15e67f46fda797dfe6ca8d101a
david.h.katz <hellodavidkatz@pm.me>
2022-11-07 08:11:11 -0500

    change dummy data to have more variety

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -5,8 +5,8 @@ import NewExpense from "./components/NewExpense/NewExpense";
 
 const DUMMY_EXPENSES = [
   {'id': 'e1', 'title': 'Car Insurance', 'amount': 268.28, 'date': new Date(2021, 2, 28)},
-  {'id': 'e2', 'title': 'Home Insurance', 'amount': 1000.28, 'date': new Date(2021, 2, 28)},
-  {'id': 'e3', 'title': 'Motorcycle Insurance', 'amount': 42.28, 'date': new Date(2021, 2, 28)},
+  {'id': 'e2', 'title': 'Home Insurance', 'amount': 1000.28, 'date': new Date(2022, 4, 28)},
+  {'id': 'e3', 'title': 'Motorcycle Insurance', 'amount': 42.28, 'date': new Date(2022, 9, 28)},
   {'id': 'e4', 'title': 'Life Insurance', 'amount': 100.28, 'date': new Date(2021, 2, 28)},
 ]
 
