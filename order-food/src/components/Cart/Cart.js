b836f946533c3350613558fc55ee4d676a6bed99
david.h.katz <hellodavidkatz@pm.me>
2022-12-19 09:16:54 -0500

    add CartContext to Cart.js

--- order-food/src/components/Cart/Cart.js
+++ order-food/src/components/Cart/Cart.js
@@ -1,12 +1,17 @@
-import React from "react";
-import Modal from "../UI/Modal";
+import { useContext } from 'react';
+import Modal from '../UI/Modal';
+import CartContext from '../../store/cart-context';
 
-import styles from "./Cart.module.css";
+import styles from './Cart.module.css';
 
 const Cart = (props) => {
+  const cartCtx = useContext(CartContext);
+  const totalAmount = `$${cartCtx.totalAmount.toFixed(2)}`;
+  const hasItems = cartCtx.items.length > 0
+
   const cartItems = (
-    <ul className={styles["cart-items"]}>
-      {[{ id: "c1", name: "dippidy do da", amount: 2, price: 12.99 }].map(
+    <ul className={styles['cart-items']}>
+      {cartCtx.items.map(
         (item) => (
           <li>{item.name}</li>
         )
@@ -19,13 +24,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount </span>
-        <span>$35.62</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles["button--alt"]} onClick={props.onClose}>
+        <button className={styles['button--alt']} onClick={props.onClose}>
           Close
         </button>
-        <button className={styles.button}>Order</button>
+        {hasItems && <button className={styles.button}>Order</button>}
       </div>
     </Modal>
   );
