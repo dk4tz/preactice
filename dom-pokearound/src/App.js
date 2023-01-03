7f1d8ca5eb9861cad20a5ff2a9a7e0d5945cc9d6
david.h.katz <hellodavidkatz@pm.me>
2023-01-03 16:23:18 -0500

    useMemo and useCallback

--- dom-pokearound/src/App.js
+++ dom-pokearound/src/App.js
@@ -1,20 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 import DummyOutput from './components/Dummy/DummyOutput';
 
 function App() {
   const [showMessage, setShowMessage] = useState(false);
+  const [allowToggle, setAllowToggle] = useState(false);
 
   console.log('APP RUNNING');
 
-  const toggleMessageHandler = () => {
-    setShowMessage((previousState) => !previousState);
-  };
+  const toggleMessageHandler = useCallback(() => {
+    if (allowToggle) {
+      setShowMessage((previousState) => !previousState);
+    }
+  }, [allowToggle]);
+
+  const allowToggleHandler = () => {
+    setAllowToggle(true);
+  }
 
   return (
     <div className='app'>
       <h1>Hello David!</h1>
       <DummyOutput show={showMessage}/>
       <button onClick={toggleMessageHandler}>Toggle hidden message</button>
+      <button onClick={allowToggleHandler}>Release ze toggle</button>
     </div>
   );
 }
