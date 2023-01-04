44e5bedbc4f2a7954b80a9ec1a7fc4bc48caffdc
david.h.katz <hellodavidkatz@pm.me>
2023-01-04 16:31:29 -0500

    movies-app - initial commit

---
+++ movies-app/src/App.js
@@ -0,0 +1,90 @@
+import React, { useState, useEffect, useCallback } from 'react';
+
+import MovieList from './components/MovieList';
+import './App.css';
+import AddMovie from './components/AddMovie';
+
+function App() {
+  const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchMoviesHandler = useCallback(async () => {
+    setIsLoading(true);
+    setError(null); // reset error state before each new call
+
+    try {
+      // const response = await fetch('https://swapi.dev/api/films');
+      const response = await fetch(
+        'https://movies-app-7bebf-default-rtdb.firebaseio.com/movies.json'
+      );
+
+      if (!response.ok) {
+        throw new Error('BIG ERROR ENERGY');
+      }
+
+      const data = await response.json();
+
+      const loadedMovies = [];
+
+      for (const key in data) {
+        loadedMovies.push({
+          id: key,
+          title: data[key].title,
+          openingText: data[key].openingText,
+          releaseDate: data[key].releaseDate
+        })
+      }
+      setMovies(loadedMovies);
+      fetchMoviesHandler();
+    } catch (error) {
+      setError(error.message);
+    }
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
+
+  const addMovieHanlder = async (movie) => {
+    const response = await fetch(
+      'https://movies-app-7bebf-default-rtdb.firebaseio.com/movies.json',
+      {
+        method: 'POST',
+        body: JSON.stringify(movie),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+    const data = await response.json();
+    // could also add error handling here
+    console.log(data);
+  };
+
+  let content = <p>Press the button to get the movies.</p>;
+  if (movies.length > 0) {
+    content = <MovieList movies={movies} />;
+  }
+  if (error) {
+    content = <p>{error}</p>;
+  }
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  return (
+    <>
+      <section>
+        <AddMovie onAddMovie={addMovieHanlder} />
+      </section>
+      <section>
+        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
+      </section>
+      <section>{content}</section>
+    </>
+  );
+}
+
+export default App;
