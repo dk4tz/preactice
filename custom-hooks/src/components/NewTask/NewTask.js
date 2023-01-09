a8a34ab26bc714caca996fec55668ef90e05bb0f
david.h.katz <hellodavidkatz@pm.me>
2023-01-09 17:09:29 -0500

    custom-hooks - initial commit

---
+++ custom-hooks/src/components/NewTask/NewTask.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import Section from '../UI/Section';
+import TaskForm from './TaskForm';
+import useHttp from '../../hooks/useHttp';
+
+const NewTask = (props) => {
+  const { isLoading, error, sendRequest: addTaskRequest } = useHttp();
+
+  const createTask = (taskText, taskData) => {
+    const generatedId = taskData.name; // firebase-specific => "name" contains generated id
+    const createdTask = { id: generatedId, text: taskText };
+
+    props.onAddTask(createdTask);
+  };
+
+  const enterTaskHandler = async (taskText) => {
+    addTaskRequest(
+      {
+        url: 'https://custom-2401e-default-rtdb.firebaseio.com/tasks.json',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: { text: taskText },
+      },
+      createTask.bind(null, taskText)
+    );
+  };
+
+  return (
+    <Section>
+      <TaskForm onEnterTask={enterTaskHandler} loading={isLoading} />
+      {error && <p>{error}</p>}
+    </Section>
+  );
+};
+
+export default NewTask;
---
+++ custom-hooks/src/hooks/useHttp.js
@@ -0,0 +1,36 @@
+import { useState, useCallback } from 'react';
+
+const useHttp = () => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(async (requestConfig, transformDataFn) => {
+    setIsLoading(true);
+    setError(null);
+    try {
+      const response = await fetch(requestConfig.url, {
+        method: requestConfig.method ? requestConfig.method : 'GET',
+        headers: requestConfig.headers ? requestConfig.headers : {},
+        body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+      });
+
+      if (!response.ok) {
+        throw new Error('ARGH!! WRONG WRONG WRONG!');
+      }
+
+      const data = await response.json();
+      transformDataFn(data);
+    } catch (err) {
+      setError(err.message || 'ARGH!!! SUMTIN WRONG DOE');
+    }
+    setIsLoading(false);
+  }, []);
+
+  return {
+    isLoading: isLoading,
+    error: error,
+    sendRequest: sendRequest,
+  };
+};
+
+export default useHttp;
